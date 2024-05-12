18aa8c6ea1765b171699b7c9e4f3a1d901ff0c1b
Nikolay Kostadinov <nikolaykostadinov21@gmail.com>
2024-05-12 13:25:56 +0300

    Remove `total_supply` variable from BronzeToken struct

--- tokens/bronze/lib.rs
+++ tokens/bronze/lib.rs
@@ -11,23 +11,20 @@ mod bronze_token {
     #[derive(Default)]
     pub struct BronzeToken {
         token: Erc20,
-        total_supply: Balance,
     }
 
     impl BronzeToken {
         #[ink(constructor)]
         pub fn mint(initial_supply: Balance) -> Self {
             let bronze_token = Erc20::new(initial_supply);
-            let bronze_token_supply = bronze_token.total_supply();
             Self {
                 token: bronze_token,
-                total_supply: bronze_token_supply,
             }
         }
 
         #[ink(message)]
         pub fn total_supply(&self) -> Balance {
-            self.total_supply
+            self.token.total_supply()
         }
     }
 }
