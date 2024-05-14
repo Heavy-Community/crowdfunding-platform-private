e7300464d67ee73bbc221f5390caf226febe2141
Emil Tsanev <emiltsanev2001@gmail.com>
2024-05-14 12:29:44 +0300

    actually transfer funds with only address of ERC20 token - real implementation

--- faucet/lib.rs
+++ faucet/lib.rs
@@ -8,89 +8,137 @@ pub use self::faucet::{
 #[ink::contract]
 mod faucet {
     use ink::storage::Mapping;
-    use erc20::Erc20Ref;
+    use ink::env::{DefaultEnvironment, CallFlags};
+    use ink::env::call::{build_call, ExecutionInput, Selector};
 
-    // Different tokens' amounts that will faucet give to investors
-    const BRONZE_TOKENS_AMOUNT: u128 = 1_000_000_000;
-    const SILVER_TOKENS_AMOUNT: u128 = 100_000;
-    const GOLD_TOKENS_AMOUNT: u128 = 1000;
+    /// The Faucet error types.
+    #[derive(Debug, PartialEq, Eq)]
+    #[ink::scale_derive(Encode, Decode, TypeInfo)]
+    pub enum Error {
+        /// Returned if the address of the token is already added
+        TokenAlreadyAdded,
+        /// Returned if the address is not added as a token contract in the Faucet
+        TokenNotFound,
+        /// Returned if user is not allowed to get free tokens_withdraw_amount at the current time
+        UserNotAllowedToWithdraw,
+        /// Returned if there was a transaction error in the custom tokens_withdraw_amount
+        TransferFailed,
+        /// Returned if contract couldn't calculate the next time that user will be allowed to use
+        /// faucet (shouldn't happen!!)
+        NextAccessTimeCalculation,
+    }
 
     /// 1 hour
     const WAIT_TIME: Timestamp = 3600;
 
-    // Here token instances will be connected after Niko's implementation
-    // Below is solidity token example
-    // ERC20 public tokenInstance; -> Will be used in the constructor
+    /// The Faucet result type.
+    pub type Result<T> = core::result::Result<T, Error>;
+
+    /// Account to token hash, used for a key in the mapping
+    pub type AccountTokenPair = (AccountId, AccountId);
 
     #[ink(storage)]
     pub struct Faucet {
-        /// Map with Address <-> Last time this address has requested funds
-        /// (TBD) Right now - no matter what token you have requested, u will wait 1 hour
-        last_access_time: Mapping<AccountId, Timestamp>,
-        tokens : Erc20Ref,
+        /// Map with user and requested token pairs, which holds the next
+        /// possible time for withdrawing funds
+        next_access_times: Mapping<AccountTokenPair, Timestamp>,
+
+        /// Different tokens' address and amount that the Faucet will give to users
+        tokens_withdraw_amount: Mapping<AccountId, Balance>,
     }
 
     impl Faucet {
         /// Creates a new Faucet contract
         #[ink(constructor)]
-        pub fn new(token_contract: Hash, token_total_supply: Balance) -> Self {
-            let token_contract = Erc20Ref::new(token_total_supply)
-                .code_hash(token_contract)
-                .endowment(0)
-                .salt_bytes([0xDE, 0xAD, 0xBE, 0xEF])
-                .instantiate();
+        pub fn new() -> Self {
             Self {
-                last_access_time: Mapping::new(),
-                tokens: token_contract,
+                next_access_times: Mapping::new(),
+                tokens_withdraw_amount: Mapping::new(),
             }
         }
 
-        /// Give Bronze tokens to user if he/she is eligible
+        /// Insert new token type that the faucet can "give"
         #[ink(message)]
-        pub fn request_bronze_tokens(&mut self, user: AccountId) {
-            // TODO: Delete the current impl and do it for BronzeToken
-            // TODO: Check if there is sth like require(..) in Ink!
-            if self.is_allowed_to_withdraw_impl(&user) {
-                let _ = self.tokens.transfer(user, BRONZE_TOKENS_AMOUNT);
-                self.last_access_time.insert(user, &self.env().block_timestamp());
+        pub fn add_token_type(&mut self, token_contract: AccountId, withdrawing_amount: Balance) -> Result<()> {
+            if self.tokens_withdraw_amount.contains(token_contract) {
+                return Err(Error::TokenAlreadyAdded);
+            }
+            self.tokens_withdraw_amount.insert(token_contract, &withdrawing_amount);
+            Ok(())
+        }
+
+        /// Give tokens to user if he/she is eligible
+        #[ink(message)]
+        pub fn request_tokens(&mut self, token_contract: AccountId) -> Result<()> {
+            if !self.tokens_withdraw_amount.contains(token_contract) {
+                return Err(Error::TokenNotFound);
+            }
+
+            if self.is_allowed_to_withdraw_impl(&self.env().caller(), &token_contract) {
+                if let Some(withdrawing_amount) = self.tokens_withdraw_amount.get(&token_contract) {
+                    let mut call_flags = ink::env::CallFlags::empty();
+                    call_flags.set(CallFlags::TAIL_CALL, true);
+
+                    let transfer_result = build_call::<DefaultEnvironment>()
+                        .call(token_contract)
+                        .call_v1()
+                        .gas_limit(0)
+                        .transferred_value(0)
+                        .call_flags(call_flags)
+                        .exec_input(
+                            ExecutionInput::new(Selector::new(ink::selector_bytes!("transfer")))
+                            .push_arg(self.env().caller()) // To
+                            .push_arg(withdrawing_amount) // Amount
+                        )
+                        .returns::<Result<()>>()
+                        .try_invoke();
+
+                    // TODO: Somehow we do not contiue with our function -> do not print KUR, do
+                    //       not insert in the next_access_times
+                    ink::env::debug_println!("KUR");
+
+                    if transfer_result.is_err() {
+                        return Err(Error::TransferFailed);
+                    }
+
+                    // TODO: Research/discuss whether it is better to use assert!(...) instead of
+                    //       returning Error gracefully (in Solidity, you would use require(...))
+                    if let Some(caller_next_access_time) = self.env().block_timestamp().checked_add(WAIT_TIME) {
+                        ink::env::debug_println!("Next access time: {}; Current time: {}", caller_next_access_time, self.env().block_timestamp());
+                        self.next_access_times.insert((&self.env().caller(), &token_contract), &caller_next_access_time);
+                        ink::env::debug_println!("Does it contain: {}", !self.next_access_times.contains((&self.env().caller(), &token_contract)));
+                        Ok(())
+                    }
+                    else {
+                        Err(Error::NextAccessTimeCalculation)
+                    }
+                }
+                else {
+                    Err(Error::TokenNotFound)
+                }
+            }
+            else {
+                Err(Error::UserNotAllowedToWithdraw)
             }
         }
 
-        /// Give Silver tokens to user if he/she is eligible
-        #[ink(message)]
-        pub fn request_silver_tokens(&mut self) {
-        }
-
-        /// Give Gold tokens to user if he/she is eligible.
-        #[ink(message)]
-        pub fn request_gold_tokens(&mut self) {
-        }
-
-        /// Get balance of custom tokens of an account
-        #[ink(message)]
-        pub fn balance_of_user(&mut self, user: AccountId) -> Balance  {
-            self.tokens.balance_of(user)
-        }
-
-        // TODO: Rework to be more detailed and "safe"
         /// Check whether the user of our Faucet contract is eligible
         /// to request new funds based on his/her last access time.
         #[ink(message)]
-        pub fn is_allowed_to_withdraw(&self, user: AccountId) -> bool {
-            return self.is_allowed_to_withdraw_impl(&user);
+        pub fn is_allowed_to_withdraw(&self, token_contract: AccountId) -> bool {
+            self.is_allowed_to_withdraw_impl(&self.env().caller(), &token_contract)
         }
 
         #[inline]
-        fn is_allowed_to_withdraw_impl(&self, user: &AccountId) -> bool {
-            if !self.last_access_time.contains(user) {
+        fn is_allowed_to_withdraw_impl(&self, user: &AccountId, token_contract: &AccountId) -> bool {
+            if !self.next_access_times.contains((user, token_contract)) {
                 return true;
             }
-            else if let Some(user_last_access_time) = self.last_access_time.get(user) {
-                // TODO: Make it SAFE!!!
-                let next_possible_time = user_last_access_time.checked_add(WAIT_TIME).unwrap();
-                return next_possible_time <= self.env().block_timestamp();
+            else if let Some(next_access_time) = self.next_access_times.get((user, token_contract)) {
+                ink::env::debug_println!("Next access time: {}; Current time: {}", next_access_time, self.env().block_timestamp());
+                return next_access_time <= self.env().block_timestamp();
             }
-            return false;
+            false
         }
     }
 
