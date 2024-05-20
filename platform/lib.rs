57f2c9dce88df14897ba7b2fef0ee625e7705d34
Emil Tsanev <emiltsanev2001@gmail.com>
2024-05-20 21:31:06 +0300

    fix build + more functinality wip

--- platform/lib.rs
+++ platform/lib.rs
@@ -5,15 +5,20 @@ mod platform {
 
     use ink::storage::Mapping;
 
+    // The token
+    use erc20::Erc20Ref;
+
     /// The Faucet error types.
     #[derive(Debug, PartialEq, Eq)]
     #[ink::scale_derive(Encode, Decode, TypeInfo)]
     pub enum Error {
         GeneralError,
+        TransferError,
         NotExistingProject,
         DeadlineExceeded,
         ZeroAmount,
         FailedCalculation,
+        NotProjectOwner,
     }
 
     pub type UserTokenPair = (AccountId, AccountId);
@@ -45,7 +50,7 @@ mod platform {
     #[cfg_attr(feature = "std", derive(ink::storage::traits::StorageLayout))]
     pub struct Project {
         /// Map of all users and the amount they have invested in the project
-        investors: (AccountId, InvestorInfo),
+        // investors: (AccountId, InvestorInfo),
 
         /// All invested funds in the project
         invested_funds: Balance,
@@ -69,62 +74,9 @@ mod platform {
     }
 
     impl Project {
-        // pub fn is_investor_of_a_token(&self, user: &AccountId) -> bool {
-        //     self.investors.contains(user)
-        // }
-
-        // pub fn is_successful(&self) -> bool {
-        //     self.successful
-        // }
-
-        // Level is going to be provided from the FE
-        // pub fn invest(
-        //     &mut self,
-        //     user: &AccountId,
-        //     amount: Balance,
-        //     level: u8,
-        // ) -> ProjectResult<()> {
-        //     if self.is_successful() {
-        //         return Err(ProjectError::AlreadySuccesfull);
-        //     }
-
-        //     // First add the amount to overall project invested_funds
-        //     let invested_funds = self.invested_funds.checked_add(amount);
-        //     if invested_funds.is_none() {
-        //         return Err(ProjectError::FailedCalculation);
-        //     }
-        //     self.invested_funds = invested_funds.unwrap();
-
-        //     // Then add the amount to the corresponding user's invested_funds
-        //     if let Some(investor_info) = self.investors.get(user) {
-        //         let new_amount = investor_info.0.checked_add(amount);
-        //         let current_level = investor_info.1;
-        //         if new_amount.is_none() {
-        //             return Err(ProjectError::FailedCalculation); // shouldn't happen
-        //         }
-
-        //         self.investors.insert(user, &(new_amount.unwrap(), level));
-        //     } else {
-        //         self.investors.insert(user, &(amount, level));
-        //     }
-
-        //     Ok(())
-        // }
-
-        // pub fn get_investor_level(&self, user: &AccountId) -> ProjectResult<u8> {
-        //     if !self.investors.contains(user) {
-        //         return Err(ProjectError::NotInvestor);
-        //     }
-
-        //     let investor = self.investors.get(user).unwrap();
-        //     Ok(investor.1)
-        // }+
-
-        // fn calculate_new_inv_level(&self, current_level: u8, amount_invested: &Balance) -> u8 {
-        //     for () {
-        //         if amoun
-        //     }
-        // }
+        pub fn is_successful(&self) -> bool {
+            self.successful
+        }
     }
 
     #[ink(storage)]
@@ -132,24 +84,36 @@ mod platform {
         projects_counter: u128,
         ongoing_projects: Mapping<u128, Project>,
         investors: Mapping<(AccountId, u128), Balance>,
-        token: 
+        token: Erc20Ref,
     }
 
     impl Platform {
         #[ink(constructor)]
-        pub fn new() -> Self {
+        pub fn new(token_hash: Hash) -> Self {
+            //  TODO: Discuss how exactly we will keep the tokens here
+            // let token = Erc20Ref::new(true)
+            //     .code_hash(token_hash)
+            //     .endowment(0)
+            //     .salt_bytes([0xDE, 0xAD, 0xBE, 0xEF])
+            //     .ref_time_limit(ref_time_limit)
+            //     .proof_size_limit(proof_size_limit)
+            //     .storage_deposit_limit(storage_deposit_limit)
+            //     .instantiate();
+
             unreachable!("Constructor isn't called since we are using Delegator pattern!");
         }
 
         /// Initializes new campaign with `owner`, funding `goal` and `timeline`
         #[ink(message)]
-        pub fn initialize_campaign(&self) {}
+        pub fn initialize_campaign(&mut self, project: Project) {
+            self.projects_counter = self.projects_counter.checked_add(1).unwrap();
+            self.ongoing_projects.insert(self.projects_counter, &project);
+        }
 
         /// `investor` deposits `amount` of funds in particular `project`
         #[ink(message)]
         pub fn invest_funds(
             &mut self,
-            investor: AccountId,
             amount: Balance,
             project_id: u128,
         ) -> Result<()> {
@@ -161,23 +125,32 @@ mod platform {
                 return Err(Error::ZeroAmount);
             }
 
+            let investor = self.env().caller();
 
             if let Some(current_invested_amount) = self.investors.get((investor, project_id)) {
                 let new_invested_amount = current_invested_amount.checked_add(amount);
                 if new_invested_amount.is_none() {
                     return Err(Error::FailedCalculation)
                 }
-                let _ = self.investors.insert((investor, project_id), &new_invested_amount.unwrap());
+                self.investors.insert((investor, project_id), &new_invested_amount.unwrap());
+            }
+
+            // Transfer the invested `amount` to our address
+            let transfer_result = self.token.transfer_from(investor, self.env().account_id(), amount);
+            if transfer_result.is_err() {
+                return Err(Error::TransferError);
             }
 
             Ok(())
         }
 
         #[ink(message)]
-        pub fn withdraw_funds(&self, owner: AccountId, project_id: Project) {
-            self.is_project_owner(owner);
-            self.is_project_successful(project_id);
-            self.is_existing_project(project_id);
+        pub fn withdraw_funds(&self, owner: AccountId, project_id: u128) -> Result<()> {
+            self.is_existing_project(project_id)?;
+            self.is_project_successful(project_id)?;
+            self.is_project_owner(owner, project_id)?;
+
+            Ok(())
         }
 
         /// Investor in a `campaign` can revoke `amount` of his deposits.
@@ -187,19 +160,18 @@ mod platform {
         pub fn refund_funds(&self) {}
 
         /// Checks if the caller is the owner of the project.
-        fn is_project_owner(&self, owner: AccountId) {
-            assert_eq!(
-                self.env().caller(),
-                owner,
-                "{:?} cannot withdraw!. Only the owner ({:?}) of the project can withdraw the funds!",
-                self.env().caller(),
-                owner
-            )
+        fn is_project_owner(&self, owner: AccountId, project_id: u128) -> Result<()> {
+            if owner == self.ongoing_projects.get(project_id).unwrap().owner {
+                Ok(())
+            }
+            else {
+                Err(Error::NotProjectOwner)
+            }
         }
 
         /// Checks if project is successful.
         fn is_project_successful(&self, project_id: u128) -> Result<()> {
-            if self.ongoing_projects.get(project_id).unwrap().successful
+            if self.ongoing_projects.get(project_id).unwrap().is_successful()
             {
                 Ok(())
             } else {
