c35725b1354c5b0404de773d66b9330a4c7596bc
Emil Tsanev <emiltsanev2001@gmail.com>
2024-05-25 14:15:39 +0300

    add token type fe implementation

--- front-end/src/deployments/deployments.tsx
+++ front-end/src/deployments/deployments.tsx
@@ -7,8 +7,8 @@ export const FaucetAbi = FaucetJson as Record<string, any>;
 export const PlatformAbi = PlatformJson as Record<string, any>;
 
 export enum ContractsAddresses {
-    Token = '5FcewiPMSveFJxgxyqNmwRCvRj7nSp4HcZriyaHzk3jexFxw',
-    Faucet = '5Fm2EUXjj6oXoUooCxYYF2rGZxNXrkkwVA1noTGjn8fy9mxB',
+    Token = '5Emr6PEhmRGuExeMZ5ns2NcDLZD7yi5YyvsR15wmGCTZ3AYv',
+    Faucet = '5HQs79zc9MNSKAa37tXkhAMEm8dYJKYFDWEDpAD6dsfP9WVt',
     // TODO: Change with real address of the deployed contracts
     Platform = '5Hgiu38EojYy9DLpJJcNXer18ubxGh8bZLZv86SBf2kEmXeu',
 }
